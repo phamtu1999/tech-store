83c046162f0c9f98b084f7196497b49443f89f58
Admin <admin@test.com>
2026-04-25 20:16:28 +0700

    fix: attach Authorization token in Axios request interceptor

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -19,6 +19,10 @@ const api = axios.create({
 // Request interceptor
 api.interceptors.request.use(
   (config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
     return config
   },
   (error) => {
